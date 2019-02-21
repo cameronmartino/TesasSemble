1e673104b1836a1634b7b0d1620afafcc842488b
gwarmstrong <garmstrong@colgate.edu>
2019-02-20 17:21:31 -0800

    filled out more details of graph and algorithm

--- TesasSemble/graph.py
+++ TesasSemble/graph.py
@@ -3,11 +3,13 @@ from graph_components import Node, Edge
 class DiGraph:
 
 	def __init__(self):
-		self.nodes = []
-		self.edges = []
+		self.nodes = {} 
+		self.edges = [] 
+		self.in_neighbors = dict() # list of in_neibhors for each node
+		self.out_neighbors = dict() # list of out_neighbors for each node
 
 	def maximal_non_branching_paths(self):
-		None
+		# return list of paths (made up of graph nodes)
 		pass
 
 	def neighbor_graphs(self, super_graph, k):
@@ -19,16 +21,32 @@ class DiGraph:
 		pass
 
 	def add_edge(self, edge):
-		pass
+		nodes = edge.node_a, edge.node_b
+		for node in nodes:
+			self.add_node(node)
+
+		self.out_neighbors[nodes[0]].append(nodes[1])
+		self.in_neighbors[nodes[1]].append(nodes[0])
 
 	def add_node(self, node):
-		pass
+		if node not in self.nodes:
+			self.nodes.add(node)
+			self.in_neighbors[node] = []
+			self.out_neighbors[node] = []
 
-	def add_edges_from(self, edge):
-		pass
+	def in_degree(self, node):
+		return len(self.in_neighbors(node))
 
-	def add_nodes_from(self, edge):
-		pass
+	def out_degree(self, node):
+		return len(self.out_neighbors(node))
+
+	def add_edges_from(self, edges):
+		for edge in edges:
+			self.add_edge(edge)
+
+	def add_nodes_from(self, nodes):
+		for node in nodes:
+			self.add_node(node)
 
 	def remove_edge(self, edge):
 		pass
@@ -45,29 +63,41 @@ class DiGraph:
 	def subgraph_from_edgelist(self, edges):
 		pass
 
+	def edge_neighbors(self, edge):
+		# TODO make sure this is both edges_after and edges_before
+		pass
+
+	def edges_after(self, edge):
+		# TODO use edge object not list for edge
+		return [[edge[1], e_to] for e_to in self.out_neighbors(edge[1])
+
+	def edges_before(self, edge):
+		# TODO use edge object not list for edge
+		return [[e_from, edge[0] for e_from in self.in_neighbors(edge[0])]
+
+
 class RedBlueDiGraph(DiGraph):
 
 	def __init__(self):
 		super(RedBlueDiGraph, self).__init__()
 		self.coverage = 0
 
-	def score(self):
-		pass
+	def score(self, alpha):
+		avg_coverage = self.coverage / len(self.edges)
+		paths = self.maximal_non_branching_paths()
+		total_path_length = sum([len(path) for path in paths])
+		avg_path_length =  total_path_length/len(paths)
+		return alpha * avg_coverage + (1-alpha) * avg_path_length
 
-	def add_edge(self, edge, color):
-		pass
-
-	def add_edges_from(self, edgelist):
-		# TODO specify input form
-		pass
+	def add_edge(self, edge):
+		assert 'color' in edge.data
+		super(RedBlueDiGraph, self).add_edge(edge)
 
 	def calculate_coverage(self):
-		# TODO return sum([1 if edge.data['color'] == 'blue' for edge in self.edges])
-		pass
+		return sum([1 if edge.data['color'] == 'red' for edge in self.edges])
 
 	def subgraph_from_edgelist(self, edges):
 		subgraph = super(RedBlueDiGraph, self).subgraph_from_edgelist(edges)
 		subgraph.coverage = self.calculate_coverage()
-
-
+		return subgraph
 
--- TesasSemble/optim_utils.py
+++ TesasSemble/optim_utils.py
@@ -1,3 +1,14 @@
-import graph # TODO do we need?
+import copy
+import random
+import graph 
 
 # TODO implement graph sampling techniques
+
+def i_sample_edges(Graph, i):
+	edge_list = [edge if edge.data['color'] == 'red' for edge in Graph.edges]
+	random.shuffle(edge_list)
+	sampled_edges = edge_list[:i]
+	H = graph.RedBlueDiGraph()
+	H.add_edges_from(sampled_edges)
+	return H
+
