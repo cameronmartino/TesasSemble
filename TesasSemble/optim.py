292aaea329b91b327efea14b9d6857d01030cb39
gwarmstrong <garmstrong@colgate.edu>
2019-02-28 11:55:39 -0800

    squash optim never updating bug

--- TesasSemble/optim.py
+++ TesasSemble/optim.py
@@ -1,16 +1,19 @@
 import TesasSemble.optim_utils as optim_utils
+import TesasSemble.graph as graph
 
-def randomized_optimal_subgraph(G, i, k, alpha):
-    H = optim_utils.i_sample_edges(G, i)
-    best_H = None
-    best_score = 0
-    while best_H != H:
-        best_H = H
+def randomized_optimal_subgraph(H, G, k, alpha):
+    best_H = H 
+    flag = True
+    best_score = best_H.score(alpha)
+    while best_H != H or flag:
+        H = best_H
         H_score = H.score(alpha)
         for H_prime in H.neighbor_graphs(H, G, k):
             H_prime_score = H_prime.score(alpha)
             if H_prime_score > H_score:
                 best_H = H_prime
                 best_score = H_prime_score
+        flag = False
+
     return best_H, best_score
 
