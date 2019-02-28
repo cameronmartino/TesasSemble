ee938c77b2cc7eaf637e6642e7fb63b17758408f
gwarmstrong <garmstrong@colgate.edu>
2019-02-27 23:16:07 -0800

    squashed mnbp bug, added larger test example, small scoring test

--- tests/test_graph_class.py
+++ tests/test_graph_class.py
@@ -57,6 +57,61 @@ class TestGraphClass(unittest.TestCase):
 		del self.CD
 		del self.DA
 
+	def setup_small_diamond_graph(self):
+		self.G = RedBlueDiGraph()
+
+		self.A = Node('A')
+		self.B = Node('B')
+		self.C = Node('C')
+		self.D = Node('D')
+		self.E = Node('E')
+		self.F = Node('F')
+		self.G_ = Node('G')
+		self.H = Node('H')
+		self.I = Node('I')
+		self.J = Node('J')
+
+		self.AC = Edge('AC', self.A, self.C)
+		self.BC = Edge('BC', self.B, self.C)
+		self.CD = Edge('CD', self.C, self.D)
+		self.CF = Edge('CF', self.C, self.F)
+		self.DE = Edge('DE', self.D, self.E)
+		self.FE = Edge('FE', self.F, self.E)
+		self.EG = Edge('EG', self.E, self.G_)
+		self.EH = Edge('EH', self.E, self.H)
+		self.IJ = Edge('IJ', self.I, self.J)
+		self.JI = Edge('JI', self.J, self.I)
+
+		edge_list = [self.AC, self.BC, self.CD, self.CF, self.DE, self.FE, self.EG, self.EH, self.IJ, self.JI]
+		colors 	  = ['blue',  'red',   'red',   'blue',  'red',   'blue',  'red',   'blue',  'red',   'red'  ]
+		self.G.add_edges_from(edge_list, colors)
+
+	def teardown_small_diamond_graph(self):
+		del self.G
+
+		del self.A
+		del self.B
+		del self.C
+		del self.D
+		del self.E
+		del self.F
+		del self.G_
+		del self.H
+		del self.I
+		del self.J
+
+		del self.AC 
+		del self.BC 
+		del self.CD 
+		del self.CF 
+		del self.DE 
+		del self.FE 
+		del self.EG 
+		del self.EH 
+		del self.IJ 
+		del self.JI 
+		
+
 	def test_DiGraph_init(self):
 		G = DiGraph()
 
@@ -92,6 +147,9 @@ class TestGraphClass(unittest.TestCase):
 		H = self.G.copy()
 		H.add_edge(CD)
 
+		# check that H is a RedBlueDiGraph
+		self.assertIsInstance(H, RedBlueDiGraph)
+
 		# check nothing got added to G
 		self.assertNotIn(D, self.G.nodes)
 		self.assertNotIn(CD, self.G.edges)
@@ -119,9 +177,23 @@ class TestGraphClass(unittest.TestCase):
 		expected_path = [self.AB, self.BC, self.CD, self.DA]
 
 		# there should only be one maximal nonbranching path
-		print(paths)
 		self.assertTrue(len(paths) == 1)
-		self.assertIn(expected_path, cycle(paths[0]))
+		path = paths[0]
+		path_length = len(path)
+		path.extend(path)
+		all_paths = [path[i:i+path_length] for i in range(path_length)]
+		self.assertIn(expected_path, all_paths)
 
 		self.teardown_small_cycle_graph()
+
+	def test_mnbp_diamond_graph(self):
+		self.setup_small_diamond_graph()
+
+		paths = self.G.maximal_non_branching_paths()
+	
+		# check that all the maximal non-branching paths calculated are the same as expected
+		self.assertCountEqual([1, 1, 2, 2, 1, 1, 2], [len(path) for path in paths])
+		self.assertEqual(1 + 10/14, self.G.score(0.5)) 
+
+		self.teardown_small_diamond_graph()
 
