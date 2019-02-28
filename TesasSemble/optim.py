e2c46d94953b680bfea2acf2e04359b831822695
gwarmstrong <garmstrong@colgate.edu>
2019-02-28 13:32:23 -0800

    score is now only updated on best graph

--- TesasSemble/optim.py
+++ TesasSemble/optim.py
@@ -8,12 +8,18 @@ def randomized_optimal_subgraph(H, G, k, alpha):
     while best_H != H or flag:
         H = best_H
         H_score = H.score(alpha)
+        #print('outer loop')
         for H_prime in H.neighbor_graphs(H, G, k):
             H_prime_score = H_prime.score(alpha)
+            #print('score {}: {}'.format(H_prime, H_prime_score))
             if H_prime_score > H_score:
+                #print('updated best graph!')
                 best_H = H_prime
                 best_score = H_prime_score
+                H_score = best_score
         flag = False
+
+    print(best_H, best_score)
 
     return best_H, best_score
 
