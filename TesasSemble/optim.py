f76aba7923e8dad1ca6f0ecd1f0a02242dffaf3c
gwarmstrong <garmstrong@colgate.edu>
2019-02-27 23:14:35 -0800

    optim reflects current graph class

--- TesasSemble/optim.py
+++ TesasSemble/optim.py
@@ -1,17 +1,16 @@
-import graph
-import optim_utils
+import TesasSemble.optim_utils as optim_utils
 
 def randomized_optimal_subgraph(G, i, k, alpha):
-    H = i_sample_edges(G, i)
+    H = optim_utils.i_sample_edges(G, i)
     best_H = None
     best_score = 0
     while best_H != H:
         best_H = H
         H_score = H.score(alpha)
-        for H_prime in H.neighbor_graphs(G, k):
+        for H_prime in H.neighbor_graphs(H, G, k):
             H_prime_score = H_prime.score(alpha)
             if H_prime_score > H_score:
-                best_addition = H_prime
+                best_H = H_prime
                 best_score = H_prime_score
-    return best_addition, best_score
+    return best_H, best_score
 
