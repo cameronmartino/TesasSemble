4fe302b08faab854bb1b4b005ba647e76e064a21
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2019-03-03 17:49:52 -0800

    contig assembly with any amount of overlap

---
+++ TesasSemble/contig_assembly.py
@@ -0,0 +1,45 @@
+def EdgePathtoNodePath(edgePath):
+    """Convert a path represented edges to a path represented by respective nodes"""
+    nodePath = []
+    for path in edgePath:
+        node_path = [path[0].node_a,path[0].node_b]
+        if len(path)>1:
+            for edge in path[1:]:
+                node_path.append(edge.node_b)
+        nodePath.append(node_path)
+    return nodePath
+
+def overlapBetweenStrings(string1,string2):
+    """Brute force find overlap between suffix of string1 and prefix of string 2"""
+    i = 0
+    suffString1 = string1
+    prefString2 = string2
+    while prefString2 != suffString1:
+        i += 1
+        suffString1 = string1[i:]
+        prefString2 = string2[0:len(suffString1)]
+    return len(prefString2)-1  
+
+def stringSpelledByNodePath(path,reads_map):
+    """String spelled by a path represented by node values"""
+    string = reads_map[int(str(path[0]))]
+    for i in range(1,len(path)):
+        string1 = reads_map[int(str(path[i-1]))] 
+        # this indexing may need to be changed depending on key values (are they ints?)
+        # if it is nodes instead of ints, can get ride of int(str(... and just put path[i-1]
+        string2 = reads_map[int(str(path[i]))]
+        index = overlapBetweenStrings(string1,string2)
+        string += string2[index+1:]
+    return string
+
+def contig_assembly(graph,reads_map):
+    """ 
+    input: DiGraph object
+    output: list of contigs (strings formed by non_branching_paths)
+    """
+    contigs = []
+    nodePath = EdgePathtoNodePath(graph.maximal_non_branching_paths())
+    for path in nodePath:
+        contigs += [stringSpelledByNodePath(path,reads_map)]
+        
+    return contigs
---
+++ tests/test_contig_assembly.py
@@ -0,0 +1,94 @@
+import unittest
+
+from TesasSemble.graph import *
+from TesasSemble.graph_components import *
+# from TesasSemble.contig_assembly import contig_assembly
+
+class TestContigAssembly(unittest.TestCase):
+
+    def setup_small_diamond_graph(self):
+        self.G_ = RedBlueDiGraph()
+
+        self.A = Node(1)
+        self.B = Node(2)
+        self.C = Node(3)
+        self.D = Node(4)
+        self.E = Node(5)
+        self.F = Node(6)
+        self.G = Node(7)
+        self.H = Node(8)
+        self.I = Node(9)
+        self.J = Node(10)
+
+        self.AC = Edge('AC', self.A, self.C)
+        self.BC = Edge('BC', self.B, self.C)
+        self.CD = Edge('CD', self.C, self.D)
+        self.CF = Edge('CF', self.C, self.F)
+        self.DE = Edge('DE', self.D, self.E)
+        self.FE = Edge('FE', self.F, self.E)
+        self.EG = Edge('EG', self.E, self.G)
+        self.EH = Edge('EH', self.E, self.H)
+        self.IJ = Edge('IJ', self.I, self.J)
+        self.JI = Edge('JI', self.J, self.I)
+        
+        self.reads_map = {}
+        self.reads_map[1] = 'ThisisthestringSpellLedbiePathNode1tothatOth'
+        self.reads_map[2] = 'ThisisthestringSpellLedbiePathNode2tothatOtherN0de'
+        self.reads_map[3] = 'tothatOtherN0de#bred3'
+        self.reads_map[4] = '#bred3NowIwouldtellaninterestingstorybutIm'
+        self.reads_map[5] = 'interestingstorybutImnotcreativeLikethat'
+        self.reads_map[6] = '3anasweomeandin'
+        self.reads_map[7] = 'stingstorybutImnotcreativeLikethatPerhapsThisflawwillbeMyUltimateATCGTFailure'
+        self.reads_map[8] = 'ikethatAndYetPerhapsselfAwaritywillAllowmyselftosupersedethseeSHORTcomings'
+        self.reads_map[9] = 'ThisStringShallBeastrnageMirroryStringAlsowithSomeNonRCNTssuchasAGGGTCCC'
+        self.reads_map[10] = 'suchasAGGGTCCC___someinternalTexxtMakesoforACompleteeString____ThisStringShallBeastrnageMirroryStringAlsow'
+
+        self.expected_contigs = ['ThisisthestringSpellLedbiePathNode1tothatOtherN0de#bred3',
+                    'tothatOtherN0de#bred3NowIwouldtellaninterestingstorybutImnotcreativeLikethat',
+                    'tothatOtherN0de#bred3anasweomeandinterestingstorybutImnotcreativeLikethat',
+                    'ThisisthestringSpellLedbiePathNode2tothatOtherN0de#bred3',
+                    'interestingstorybutImnotcreativeLikethatPerhapsThisflawwillbeMyUltimateATCGTFailure',
+                    'interestingstorybutImnotcreativeLikethatAndYetPerhapsselfAwaritywillAllowmyselftosupersedethseeSHORTcomings',
+                    'suchasAGGGTCCC___someinternalTexxtMakesoforACompleteeString____ThisStringShallBeastrnageMirroryStringAlsowithSomeNonRCNTssuchasAGGGTCCC___someinternalTexxtMakesoforACompleteeString____ThisStringShallBeastrnageMirroryStringAlsow']
+
+        edge_list = [self.AC, self.BC, self.CD, self.CF, self.DE, self.FE, self.EG, self.EH, self.IJ, self.JI]
+        colors 	  = ['blue',  'red',   'red',   'blue',  'red',   'blue',  'red',   'blue',  'red',   'red'  ]
+        self.G_.add_edges_from(edge_list, colors)
+
+
+    def teardown_small_diamond_graph(self):
+        del self.G_
+        del self.sub
+        del self.reads
+
+        del self.A
+        del self.B
+        del self.C
+        del self.D
+        del self.E
+        del self.F
+        del self.G
+        del self.H
+        del self.I
+        del self.J
+
+        del self.AC 
+        del self.BC 
+        del self.CD 
+        del self.CF 
+        del self.DE 
+        del self.FE
+        del self.EG 
+        del self.EH 
+        del self.IJ 
+        del self.JI 
+
+    def test_small_diamond_graph(self):
+        self.setup_small_diamond_graph()
+
+        contigs = contig_assembly(self.G_, self.reads_map)
+
+        self.assertEquals(sorted(contigs),sorted(self.expected_contigs))
+
+        self.teardown_small_diamond_graph
+
