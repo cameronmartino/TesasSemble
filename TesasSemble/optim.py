6638f4f87c93a3b8c940de353f97879413a3813d
gwarmstrong <garmstrong@colgate.edu>
2019-03-04 12:42:49 -0800

    rewrote while loop to stop when score does not improve instead of graph not changing

--- TesasSemble/optim.py
+++ TesasSemble/optim.py
@@ -7,7 +7,8 @@ def randomized_optimal_subgraph(H, G, k, alpha):
     best_H = H
     flag = True
     best_score = best_H.score(alpha)
-    while best_H != H or flag:
+    H_score = best_score
+    while best_score != H_score or flag:
         H = best_H
         H_score = H.score(alpha)
         for H_prime in H.neighbor_graphs(H, G, k):
